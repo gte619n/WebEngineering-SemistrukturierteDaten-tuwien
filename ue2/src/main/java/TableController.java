0c74d6e48c831ed00a56242c46878f9c9e4e749e
Philip Miglinci <p.miglinci@gmail.com>
2013-04-20 14:32:40 +0200

    add blank  TableController Signed-off-by: Philip Miglinci <p.miglinci@gmail.com>

---
+++ ue2/src/main/java/TableController.java
@@ -0,0 +1,21 @@
+import Beans.Player;
+import Beans.Game;
+import javax.servlet.http.HttpSession;
+
+public class TableController {
+
+  public TableController() {
+
+  }
+
+  public Game getGame(HttpSession session) {
+
+    return null;
+  }
+
+  public Game resetGame(HttpSession session) {
+
+    return null;
+  }
+
+}
