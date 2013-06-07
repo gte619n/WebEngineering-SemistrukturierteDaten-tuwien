10ee896653d80f247f69ae7463789ed4263467b7
Philip Miglinci <p.miglinci@gmail.com>
2013-06-07 09:32:55 +0200

    finish web ue4
    Signed-off-by: Philip Miglinci <p.miglinci@gmail.com>

--- ue4/src/main/java/formel0api/PlayerStorage.java
+++ ue4/src/main/java/formel0api/PlayerStorage.java
@@ -1,6 +1,7 @@
 package formel0api;
 
 import java.util.ArrayList;
+import javax.persistence.*;
 
 public class PlayerStorage {
   private static PlayerStorage ps;
@@ -17,25 +18,31 @@ public class PlayerStorage {
     conamiPlayer.setSex("FEMALE");
     conamiPlayer.setBirthdate("1966-06-06");
     players.add(conamiPlayer);
-  }
 
-  public static PlayerStorage getInstance(){
+    // //hibernate
+    // EntityManager eManager =  Persistence.createEntityManagerFactory("lab4").createEntityManager();
+
+
+    // System.out.println("\n\n>>>>>>>>>>>>>    (pmig log)    " + eManager.createQuery("select now()", Player.class).getResultList().get(0));
+}
+
+public static PlayerStorage getInstance(){
     if(ps == null){
       ps = new PlayerStorage();
-    }
-    return ps;
   }
+  return ps;
+}
 
-  public ArrayList<Player> getPlayers() {
+public ArrayList<Player> getPlayers() {
     return this.players;
-  }
+}
 
-  public Player getComputerPlayer() {
+public Player getComputerPlayer() {
     Player computerPlayer = new Player();
     computerPlayer.setFirstname("Computer");
     computerPlayer.setUsername("Super C");
     computerPlayer.setSex("MALE");
     computerPlayer.setBirthdate("1966-06-06");
     return computerPlayer;
-  }
+}
 }
